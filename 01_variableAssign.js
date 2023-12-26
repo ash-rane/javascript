c294a566d2cc28ec1f3781b570e98b362bd4931d
ash-rane <aishwarya.rane79@gmail.com>
2023-12-26 11:04:58 +0530

    create 01_variableAssign
    added JS first program

---
+++ 01_variableAssign.js
@@ -0,0 +1,30 @@
+var myFullName;
+console.log(myFullName);
+myFullName="Aishwarya Rane";
+console.log("My Full Name:",myFullName);
+myFullName="Aishwarya Vijay Rane";
+console.log("My Full Name is:",myFullName);
+
+var myLove="I love only JS";
+console.log(myLove);
+
+var firstName="Aishwarya";
+console.log("First Name:",firstName);
+
+var lastName="Rane";
+console.log("Last Name:",lastName);
+
+var age="28";
+console.log("Age :",age);
+
+var address="Mumbai";
+console.log("Address :" ,address);
+
+var pincode="410102";
+console.log("Pincode :",pincode);
+
+var gender="Female";
+console.log("Gender :",gender);
+
+var isMarried="Married";
+console.log("Marrital Status:",isMarried);
