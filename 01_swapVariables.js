490cdf06c8515e15b4f835397fb12c24c32f4f41
ash-rane <aishwarya.rane79@gmail.com>
2023-12-27 15:09:54 +0530

    swap variables

---
+++ 01_swapVariables.js
@@ -0,0 +1,37 @@
+console.log("----------Question_01------------");
+var sweety = "Sweety";
+var cutie = "Cutie";
+
+console.log("...Before Swap...");
+console.log("Sweety",sweety);
+console.log("Cutie",cutie);
+
+teddy = sweety;
+sweety = cutie;
+cutie =teddy;
+
+console.log("...After Swap...");
+console.log("Sweety",sweety);
+console.log("Cutie",cutie);
+
+console.log("-----------Question_02-------------");
+
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+
+console.log(".........Before Swap..........");
+console.log("The num1 =>",num1);
+console.log("The num2 =>",num2);
+console.log("The num3 =>",num3);
+
+numNew = num1;
+num1 =num2;
+num2 =num3;
+num3 = numNew;
+
+console.log("\n.........After Swap..........");
+
+console.log("The num1 =>",num1);
+console.log("The num2 =>",num2);
+console.log("The num3 =>",num3);
