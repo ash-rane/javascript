e4bf22ed4d5d0eca491d13a1cf06fbcae7c5eb6e
ash-rane <aishwarya.rane79@gmail.com>
2023-12-27 14:42:00 +0530

    Create 02_variableBasicAssign
    Data type practice

---
+++ 02_variableBasicsAssign.js
@@ -0,0 +1,44 @@
+console.log("-------------Number Data Type------------------");
+var adharNumber=145784578458;
+console.log("Adhar Card Number :",adharNumber);
+console.log("The Data Type is :",typeof adharNumber);
+
+var phoneNumber=1525484254;
+console.log("The Phone Number :",phoneNumber);
+console.log("The Data Type is :",typeof phoneNumber);
+
+var chalanNumber=58456;
+console.log("The Chalan Number :",chalanNumber);
+console.log("The Data Type is :",typeof chalanNumber);
+
+
+console.log("\n-------------String Data Type------------------");
+var collegeName="Dilkap college";
+console.log("The College Name is :",collegeName)
+console.log("The Data Type is :",typeof collegeName);
+
+var cityName="Neral";
+console.log("The City Name is :",cityName)
+console.log("The Data Type is :",typeof cityName);
+
+var DistrictName="Raigad";
+console.log("The District Name is :",DistrictName)
+console.log("The Data Type is :",typeof DistrictName);
+
+console.log("\n-------------Boolean Data Type------------------");
+var isPlanForTravel=true;
+console.log("Is plan for travel ?:",isPlanForTravel);
+console.log("The Data Type is :",typeof isPlanForTravel);
+
+var isHappy=true;
+console.log("Is Happy ?:",isHappy);
+console.log("The Data Type is :",typeof isHappy);
+
+var isMarried=true;
+console.log("Is Married?:",isMarried);
+console.log("The Data Type is :",typeof isMarried);
+
+console.log("\n<------Difference between Null vs Undefined------>\n");
+
+console.log("-> A null value means that there is no any value");
+console.log("-> When the variable is declare and not assign any kind of value called as undefined")
