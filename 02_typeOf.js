d95c44229704648f45b14bca8f1781f85d153a0a
ash-rane <aishwarya.rane79@gmail.com>
2023-12-27 13:56:30 +0530

    Create 02_typeOf assign

---
+++ 02_typeOf.js
@@ -0,0 +1,29 @@
+console.log("--------Assignment_01---------\n");
+var bankName;
+console.log(bankName);
+bankName="SBI Bank";
+console.log("The Bank Name is :",bankName);
+console.log("The Type of Bank is :",typeof bankName);
+
+console.log("\n--------Assignment_02---------\n");
+
+var marks="90%";
+console.log("Marks",marks);
+console.log("Data Type of marks(operator) is :",typeof marks);
+
+console.log("\n--------Assignment_03---------\n");
+
+var isWorking;
+console.log(isWorking);
+isWorking=true;
+console.log("isWorking =",isWorking);
+console.log("The data type of isWorking(operator):",typeof isWorking);
+
+console.log("\n--------Assignment_04---------\n");
+
+var totalCount="Hundred and seven";
+console.log("Total count :",totalCount);
+console.log("The data type of operator is :",typeof totalCount);
+totalCount=107;
+console.log("Total Count",totalCount);
+console.log("The data type of operator is :",typeof totalCount);
