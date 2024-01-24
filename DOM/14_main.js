a66811a5958996850e0e62b32cb9cbbad2634daf
ash-rane <aishwarya.rane79@gmail.com>
2024-01-24 16:56:12 +0530

    DOM assignments

---
+++ DOM/14_main.js
@@ -0,0 +1,8 @@
+//const changeH1Color = document.querySelector("#mainHeading");
+//changeH1Color.innerHTML;
+//changeH1Color.style.color = "Green";
+mainHeading.style.color = "green";
+
+const projectDetails= document.querySelector("#projectDetails");
+console.log(projectDetails.innerHTML);
+projectDetails.innerHTML = "Project Description : React.js is mostly used to build interactive UI components";
